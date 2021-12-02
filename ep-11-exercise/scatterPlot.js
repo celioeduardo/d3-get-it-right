993b2132694b8da14aebeefb09dcc8035f377f81
Célio Eduardo <celioeduardo@me.com>
2021-12-02 12:47:46 -0300

    Prettier format

--- ep-11-exercise/scatterPlot.js
+++ ep-11-exercise/scatterPlot.js
@@ -1,4 +1,11 @@
-import { scaleLinear, extent, axisLeft, axisBottom, transition, scalePoint } from 'd3';
+import {
+  scaleLinear,
+  extent,
+  axisLeft,
+  axisBottom,
+  transition,
+  scalePoint,
+} from "d3";
 
 export const scatterPlot = () => {
   let width;
@@ -12,143 +19,139 @@ export const scatterPlot = () => {
   let yLabel;
 
   const my = (selection) => {
-
     let x;
-    
-    if (xLabel.startsWith('Species'))
-      x = scalePoint()
-        .domain(data.map(xValue))
-        .padding(1)
-        .round(true);
-    else
-      x = scaleLinear()
-        .domain(extent(data, xValue));
+
+    if (xLabel.startsWith("Species"))
+      x = scalePoint().domain(data.map(xValue)).padding(1).round(true);
+    else x = scaleLinear().domain(extent(data, xValue));
 
     x.range([margin.left, width - margin.right]);
 
     let y;
-    if (yLabel.startsWith('Species'))
-      y = scalePoint()
-        .domain(data.map(yValue))
-        .padding(0.2)
-        .round(true);
-    else
-      y = scaleLinear()
-        .domain(extent(data, yValue));
+    if (yLabel.startsWith("Species"))
+      y = scalePoint().domain(data.map(yValue)).padding(0.2).round(true);
+    else y = scaleLinear().domain(extent(data, yValue));
 
     y.range([height - margin.bottom, margin.top]);
-        
 
     // Data processing
-    const marks = data.map(d => ({
+    const marks = data.map((d) => ({
       x: x(xValue(d)),
       y: y(yValue(d)),
-      title: `( ${xValue(d)} , ${yValue(d)} )`
+      title: `( ${xValue(d)} , ${yValue(d)} )`,
     }));
 
     const t = transition().duration(1000);
 
     const initializeRadius = (circles) => {
-      circles.attr('r', 0);
-    }
-    
+      circles.attr("r", 0);
+    };
+
     const growRadius = (circles) => {
-      circles.transition(t).attr('r', radius);
-    }
+      circles.transition(t).attr("r", radius);
+    };
 
     const positionCircles = (circles) => {
-      circles.attr('cx', d => d.x).attr('cy', d => d.y);
-    }
+      circles.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    };
 
     // Rendering
-    const circles = selection.selectAll('circle')
+    const circles = selection
+      .selectAll("circle")
       .data(marks)
       .join(
-        (enter) => enter
-          .append('circle')
-          .call(initializeRadius)
-          .call(positionCircles)
-          .call(growRadius),
-        (update) => update
-          .call(update => update
-            .transition(t)
-            .delay((d, i) => i * 15)
+        (enter) =>
+          enter
+            .append("circle")
+            .call(initializeRadius)
             .call(positionCircles)
+            .call(growRadius),
+        (update) =>
+          update.call((update) =>
+            update
+              .transition(t)
+              .delay((d, i) => i * 15)
+              .call(positionCircles)
           ),
-        (exit) => exit.remove());
+        (exit) => exit.remove()
+      );
 
     selection
-      .selectAll('.y-axis')
+      .selectAll(".y-axis")
       .data([null])
-      .join('g')
-      .attr('class', 'y-axis')
-      .attr('transform', `translate(${margin.left},0)`)
+      .join("g")
+      .attr("class", "y-axis")
+      .attr("transform", `translate(${margin.left},0)`)
       .call(axisLeft(y))
-      .selectAll('.y-label').data([null]).join('text')
-      .attr('class','y-label')
-      .attr('fill','black')
-      .attr('text-anchor','middle')
-      .attr('transform',`rotate(-90)`)
-      .attr('x',-height/2)
-      .attr('y',-40)
+      .selectAll(".y-label")
+      .data([null])
+      .join("text")
+      .attr("class", "y-label")
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+      .attr("transform", `rotate(-90)`)
+      .attr("x", -height / 2)
+      .attr("y", -40)
       .text(yLabel);
 
     const xAxis = selection
-      .selectAll('.x-axis')
+      .selectAll(".x-axis")
       .data([null])
-      .join('g')
-      .attr('class', 'x-axis')
-      .attr('transform', `translate(0,${height - margin.bottom})`)
-      .call(xAxis => xAxis.transition(t).call(axisBottom(x)));
-      
-    xAxis.selectAll('.x-label').data([null]).join('text')
-      .attr('class','x-label')
-      .attr('fill','black')
-      .attr('x',width/2)
-      .attr('y', 40)
-      .transition(t)
-      .attr('y', height + 100)
-      .transition(t)
-      .attr('y', 40)
-      .text(xLabel);
+      .join("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call((xAxis) => xAxis.transition(t).call(axisBottom(x)));
 
+    xAxis
+      .selectAll(".x-label")
+      .data([null])
+      .join("text")
+      .attr("class", "x-label")
+      .attr("fill", "black")
+      .attr("x", width / 2)
+      .attr("y", 40)
+      .transition(t)
+      .attr("y", height + 100)
+      .transition(t)
+      .attr("y", 40)
+      .text(xLabel);
   };
 
   my.width = function (_) {
-    return arguments.length ? (width = +_, my) : width;
+    return arguments.length ? ((width = +_), my) : width;
   };
 
   my.height = function (_) {
-    return arguments.length ? (height = +_, my) : height;
-  }
+    return arguments.length ? ((height = +_), my) : height;
+  };
 
   my.data = function (_) {
-    return arguments.length ? (data = _, my) : data;
-  }
+    return arguments.length ? ((data = _), my) : data;
+  };
 
   my.xValue = function (_) {
-    return arguments.length ? (xValue = _, my) : xValue;
-  }
+    return arguments.length ? ((xValue = _), my) : xValue;
+  };
 
   my.yValue = function (_) {
-    return arguments.length ? (yValue = _, my) : yValue;
-  }
+    return arguments.length ? ((yValue = _), my) : yValue;
+  };
 
   my.margin = function (_) {
-    return arguments.length ? (margin = _, my) : margin;
-  }
+    return arguments.length ? ((margin = _), my) : margin;
+  };
 
   my.radius = function (_) {
-    return arguments.length ? (radius = +_, my) : radius;
-  }
+    return arguments.length ? ((radius = +_), my) : radius;
+  };
 
   my.xLabel = function (_) {
-    return arguments.length ? (xLabel = _, my) : xLabel;
-  }
+    return arguments.length ? ((xLabel = _), my) : xLabel;
+  };
 
   my.yLabel = function (_) {
-    return arguments.length ? (yLabel = _, my) : yLabel;
-  }
+    return arguments.length ? ((yLabel = _), my) : yLabel;
+  };
 
   return my;
-}
+};
